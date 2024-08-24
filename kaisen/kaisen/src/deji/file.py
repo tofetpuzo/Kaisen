0135fbec0d356aae50c976ce82ab8f78944d6a78
Adedeji.Awolesi <scopnatinc@hotmail.com>
2024-08-24 18:21:48 +0100

    create functions

--- kaisen/kaisen/src/deji/file.py
+++ kaisen/kaisen/src/deji/file.py
@@ -1,1 +1,9 @@
 print ("hello world, my name is Adedeji Awolesi")
+
+#Create a function
+def result(a,b):
+    result = a + b
+    return result
+print(result(2,3))
+
+#Write a function that calls another function
