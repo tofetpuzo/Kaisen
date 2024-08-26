7b10f7e41eb4b6b239792d313edb0c64e900be98
Raliat <belloraliat500@gmail.com>
2024-08-26 17:08:41 +0100

    Adding project to GitHub repo

--- kaisen/kaisen/kaisen/src/raliat/addition.py
+++ kaisen/kaisen/kaisen/src/raliat/addition.py
@@ -1,6 +1,6 @@
 def sum_numbers(num1, num2):
     total = num1 + num2
     return total 
-result = sum_numbers(20, 20)
+result = sum_numbers(2, 20)
 
-print("total is", result)
+print("total is =", result)
