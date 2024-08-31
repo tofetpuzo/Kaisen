237d164d6178e8b9012cac81c81fcaf9d4e3cc70
Owoade Taiwo <adexxy@live.com>
2024-08-31 17:12:22 +0100

    added nested function

---
+++ kaisen/kaisen/src/taiwo/nested_function.py
@@ -0,0 +1,11 @@
+def call(a,b):
+    suml = a + b 
+    return suml
+print(call(2,3))
+    
+def subtr(a,b,c):
+    d = call(a,b)
+    total = d - c
+    return total   
+ 
+print(subtr(120,340,100))
