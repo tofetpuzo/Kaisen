0e502ee0960148a9a077e509357b1b44b2c5d061
Owoade Taiwo <adexxy@live.com>
2024-09-03 15:41:39 +0100

    using python built-in function

---
+++ kaisen/kaisen/src/taiwo/in-built-function.py
@@ -0,0 +1,5 @@
+list = ['television', 'radio', 'telephone', 'computer', 'mouse', 'keyboard', 'laptop', 'monitor', 'remote', 'hard-drive', 'momory', 'track-pad']
+
+print(sorted(list))
+
+print(sorted(list, reverse=True))
