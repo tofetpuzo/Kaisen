bbc1e3bc7458500b0e7f18c4975083cd4037d3da
Adedeji.Awolesi <scopnatinc@hotmail.com>
2024-09-07 13:06:13 +0100

    Add assignments

--- kaisen/kaisen/src/deji/file.py
+++ kaisen/kaisen/src/deji/file.py
@@ -8,6 +8,13 @@ print(add(2,3))
 
 #Write a function that calls another function
 def subtract(a,b):
-    difference = add(a,b) - b
-    return a-b, difference (a,b)
-print(subtract(5,2))
+    difference = add(a,b)
+    diff = difference - b
+    return diff
+print(subtract(3,2))
+
+
+#Write a list of python store that store all things you wants, preferabley names
+list = [1,2,3,4,5,6,7,8,9,10]
+
+print(len(list))
